227997b14c3aa818c3da23fc58849b683436bfbc
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-05-30 17:18:17 +0200

    Cambios tontos

--- prueba.py
+++ prueba.py
@@ -14,7 +14,7 @@ with f as csvfile:
         if i>=21:
             dataset.append(line)
         i+=1
-       
+
 print("Comprobamos las características que tienen más de un 70% de NAs...")
 dataset = np.array(dataset)
 deberiamos_quitarla =[False]*len(dataset[0])
@@ -27,7 +27,7 @@ for d in dataset:
 for i in range(len(numeros_na)):
     if (numeros_na[i]/len(dataset))>0.7:
         deberiamos_quitarla[i]=True
-        
+
 
 indices =[]
 for deberiamos,index in zip(deberiamos_quitarla,range(len(deberiamos_quitarla))):
@@ -35,7 +35,7 @@ for deberiamos,index in zip(deberiamos_quitarla,range(len(deberiamos_quitarla)))
         indices.append(index)
         print("Debemos quitar la característica " + str(index))
 print("")
-        
+
 dataset1 = np.delete(dataset,indices,1)
 print("Número de características original: " + str(len(dataset[0])))
 print("Número de características nuevo: " + str(len(dataset1[0])))
@@ -48,7 +48,7 @@ for i in range(len(dataset1)):
             num_nas+=1
     if num_nas/len(dataset1[i])>0.15 and dataset[i][0]=="neg":
         instancias_malas.append(i)
-        
+
 print("\n\n\nTenemos " + str(len(instancias_malas)) + " instancias con más de un 15% de NAs")
 
 dataset2 = np.delete(dataset,instancias_malas,0)
