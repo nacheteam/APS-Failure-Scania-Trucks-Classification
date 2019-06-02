c998838a1350cf233d415b15b9df13129cf202f1
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-06-02 17:02:30 +0200

    Mejor estructurado el codigo

--- src/modelos.py
+++ src/modelos.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import preprocesamiento
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
 from sklearn.externals import joblib
@@ -9,6 +10,7 @@ from imblearn.over_sampling import SMOTE
 from sklearn import svm
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn import linear_model
 
 np.random.seed(123456789)
 
@@ -68,178 +70,6 @@ def plot_confusion_matrix(y_true, y_pred, classes,normalize=False,title=None,cma
     fig.tight_layout()
     return ax
 
-def obtenerDatosTrain(imputacion):
-    '''
-    @brief Función que lee los datos, los preprocesa e imputa valores perdidos
-    según la estrategia pasada por el argumento imputacion
-    @param imputacion Cadena de texto que puede ser <media> o <mediana>
-    para indicar la estrategia de imputación de valores perdidos a usar
-    @return Devuelve dos numpy arrays, el primero es el conjunto de datos y el
-    segundo las etiquetas.
-    '''
-    # Abrimos el fichero
-    f = open('../dataset/aps_failure_training_set.csv', 'r')
-    dataset = []
-    # Lo leemos como un fichero csv
-    with f as csvfile:
-        file = csv.reader(csvfile, delimiter=',', quotechar='|')
-        i=0
-        for line in file:
-            # Sabemos que el contenido está a partir de la línea 21
-            if i>=21:
-                dataset.append(line)
-            i+=1
-    # Lo convertimos en numpy array
-    dataset = np.array(dataset)
-    deberiamos_quitarla =[False]*len(dataset[0])
-    numeros_na = [0]*len(dataset[0])
-    # Estudiamos el número de NA's por característica
-    for d in dataset:
-        for i in range(len(d)):
-            if "na" in d[i]:
-                numeros_na[i]+=1
-
-    # Si tiene más de un 70% de NA's entonces la marcamos para quitarla
-    for i in range(len(numeros_na)):
-        if (numeros_na[i]/len(dataset))>0.7:
-            deberiamos_quitarla[i]=True
-
-
-    indices =[]
-    # Calculamos los índices a quitar
-    for deberiamos,index in zip(deberiamos_quitarla,range(len(deberiamos_quitarla))):
-        if deberiamos:
-            indices.append(index)
-
-    # Eliminamos los índices de las características deficientes (en cuanto a número de NA's)
-    dataset1 = np.delete(dataset,indices,1)
-
-    # Obtenemos las instancias con más de un 15% de los atributos como NA's
-    instancias_malas = []
-    for i in range(len(dataset1)):
-        num_nas = 0
-        for v in dataset1[i]:
-            if "na" in v:
-                num_nas+=1
-        if num_nas/len(dataset1[i])>0.15 and dataset[i][0]=="neg":
-            instancias_malas.append(i)
-
-    # Quitamos las instancias que aportan poca información
-    dataset2 = np.delete(dataset1,instancias_malas,0)
-
-    # Convertimos los datos a formato numpy
-    for i in range(len(dataset2)):
-        nueva_fila = []
-        for v in dataset2[i]:
-            if "na" in v:
-                nueva_fila.append(np.nan)
-            #CONVIERTO LAS ETIQUETAS A NÚMEROS
-            elif 'neg' in v:
-                nueva_fila.append(-1)
-            elif 'pos' in v:
-                nueva_fila.append(1)
-            else:
-                nueva_fila.append(float(v))
-        dataset2[i]=nueva_fila
-
-    sm = SMOTE(sampling_strategy=0.4,random_state = 123456789)
-    # Se aplica la imputación de valores según la media
-    if imputacion=="media":
-        dataset_media = []
-        imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-        dataset_media = imp.fit_transform(dataset2)
-        X_res, y_res = sm.fit_resample(dataset_media[:,1:],dataset_media[:,0])
-        return X_res, y_res
-    # Se aplica la imputación de valores según la mediana
-    elif imputacion=="mediana":
-        dataset_mediana = []
-        imp = SimpleImputer(missing_values=np.nan, strategy='median')
-        dataset_mediana = imp.fit_transform(dataset2)
-        X_res, y_res = sm.fit_resample(dataset_mediana[:,1:],dataset_mediana[:,0])
-        return X_res, y_res
-    # No se aplica imputación de valores y se devuelve el conjunto de datos con NA's
-    else:
-        print("No se ha elegido un método de imputación, devolvemos el dataset bruto.")
-        return dataset2[:,1:], dataset2[:,0]
-
-def obtenerDatosTest(imputacion):
-    '''
-    @brief Función que lee los datos de test
-    @param imputacion Cadena de texto que puede ser <media> o <mediana>
-    para indicar la estrategia de imputación de valores perdidos a usar
-    @return Devuelve dos numpy arrays, el primero el conjunto de datos y el segundo
-    las etiquetas del mismo.
-    '''
-    # Abrimos el fichero
-    f = open('../dataset/aps_failure_test_set.csv', 'r')
-    dataset = []
-    # Lo leemos como un fichero csv
-    with f as csvfile:
-        file = csv.reader(csvfile, delimiter=',', quotechar='|')
-        i=0
-        for line in file:
-            # Sabemos que el contenido está a partir de la línea 21
-            if i>=21:
-                dataset.append(line)
-            i+=1
-    # Lo convertimos en numpy array
-    dataset = np.array(dataset)
-
-    deberiamos_quitarla =[False]*len(dataset[0])
-    numeros_na = [0]*len(dataset[0])
-    # Estudiamos el número de NA's por característica
-    for d in dataset:
-        for i in range(len(d)):
-            if "na" in d[i]:
-                numeros_na[i]+=1
-
-    # Si tiene más de un 70% de NA's entonces la marcamos para quitarla
-    for i in range(len(numeros_na)):
-        if (numeros_na[i]/len(dataset))>0.7:
-            deberiamos_quitarla[i]=True
-
-
-    indices =[]
-    # Calculamos los índices a quitar
-    for deberiamos,index in zip(deberiamos_quitarla,range(len(deberiamos_quitarla))):
-        if deberiamos:
-            indices.append(index)
-
-    # Eliminamos los índices de las características deficientes (en cuanto a número de NA's)
-    dataset1 = np.delete(dataset,indices,1)
-
-    # Convertimos los datos a formato numpy
-    for i in range(len(dataset1)):
-        nueva_fila = []
-        for v in dataset1[i]:
-            if "na" in v:
-                nueva_fila.append(np.nan)
-            #CONVIERTO LAS ETIQUETAS A NÚMEROS
-            elif 'neg' in v:
-                nueva_fila.append(-1)
-            elif 'pos' in v:
-                nueva_fila.append(1)
-            else:
-                nueva_fila.append(float(v))
-        dataset1[i]=nueva_fila
-
-    # Se aplica la imputación de valores según la media
-    if imputacion=="media":
-        dataset_media = []
-        imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-        dataset_media = imp.fit_transform(dataset1)
-        return dataset_media[:,1:], dataset_media[:,0]
-    # Se aplica la imputación de valores según la mediana
-    elif imputacion=="mediana":
-        dataset_mediana = []
-        imp = SimpleImputer(missing_values=np.nan, strategy='median')
-        dataset_mediana = imp.fit_transform(dataset1)
-        return dataset_mediana[:,1:], dataset_mediana[:,0]
-    # No se aplica imputación de valores y se devuelve el conjunto de datos con NA's
-    else:
-        print("No se ha elegido un método de imputación, devolvemos el dataset bruto.")
-        return dataset1[:,1:], dataset1[:,0]
-
 def ajustaSVM(dataset_train,labels_train, ficherosvm=None, ficherosave=None):
     if ficherosvm==None:
         clasificador_svm = svm.SVC(gamma="auto", verbose=True)
@@ -251,7 +81,7 @@ def ajustaSVM(dataset_train,labels_train, ficherosvm=None, ficherosave=None):
         clasificador_svm = joblib.load(ficherosvm)
         return clasificador_svm
 
-def obtenScores(clasificador, dataset_test, labels_test):
+def obtenScores(clasificador, dataset_test, labels_test, nombre="SGD"):
     pred = clasificador.predict(dataset_test)
     score = clasificador.score(dataset_test, labels_test)
     print("El score es: " + str(score))
@@ -260,12 +90,28 @@ def obtenScores(clasificador, dataset_test, labels_test):
     print("F1 Score: " + str(metrics.f1_score(labels_test,pred,average="weighted")))
     print("Matriz de confusión")
     nombres = ["neg","pos"]
-    plot_confusion_matrix(labels_test, pred, classes=nombres,normalize = False,title='Matriz de confusión para SVM')
+    plot_confusion_matrix(labels_test, pred, classes=nombres,normalize = False,title='Matriz de confusión para ' + nombre)
     plt.show()
 
 print("Leyendo los datasets")
-dataset_train, labels_train = obtenerDatosTrain(imputacion="mediana")
-dataset_test, labels_test = obtenerDatosTest(imputacion="mediana")
+dataset_train, labels_train = preprocesamiento.obtenerDatosTrain(imputacion="mediana")
+dataset_test, labels_test = preprocesamiento.obtenerDatosTest(imputacion="mediana")
+print("########################################################################\n\n")
+
+'''
+clf = svm.LinearSVC(random_state=123456789, tol=1e-5, verbose=True)
+clf.fit(dataset_train, labels_train)
+obtenScores(clf,dataset_test, labels_test, nombre="LinearSVM")
+'''
+
+
+clf = linear_model.SGDClassifier(max_iter=10000, tol=1e-6, verbose=True)
+clf.fit(dataset_train, labels_train)
+obtenScores(clf,dataset_test, labels_test)
+
+
+'''
 print("Ajustando SVM")
 clasificador_svm = ajustaSVM(dataset_train, labels_train, ficherosave="svm_mediana.txt")
 obtenScores(clasificador_svm,dataset_test, labels_test)
+'''
--- src/preprocesamiento.py
+++ src/preprocesamiento.py
@@ -9,109 +9,125 @@ from sklearn.manifold import TSNE
 
 np.random.seed(123456789)
 
-print("Leyendo el fichero...\n\n\n")
-f = open('../dataset/aps_failure_training_set.csv', 'r')
-dataset = []
-with f as csvfile:
-    file = csv.reader(csvfile, delimiter=',', quotechar='|')
-    i=0
-    for line in file:
-        if i>=21:
-            dataset.append(line)
-        i+=1
+def leeFichero(fichero="../dataset/aps_failure_training_set.csv"):
+    print("Leyendo el fichero...\n\n\n")
+    f = open('../dataset/aps_failure_training_set.csv', 'r')
+    dataset = []
+    with f as csvfile:
+        file = csv.reader(csvfile, delimiter=',', quotechar='|')
+        i=0
+        for line in file:
+            if i>=21:
+                dataset.append(line)
+            i+=1
+    return dataset
 
-print("Comprobamos las características que tienen más de un 70% de NAs...")
-dataset = np.array(dataset)
-deberiamos_quitarla =[False]*len(dataset[0])
-numeros_na = [0]*len(dataset[0])
-for d in dataset:
-    for i in range(len(d)):
-        if "na" in d[i]:
-            numeros_na[i]+=1
+def eliminaCaracteristicasMalas(dataset):
+    print("Comprobamos las características que tienen más de un 70% de NAs...")
+    dataset = np.array(dataset)
+    deberiamos_quitarla =[False]*len(dataset[0])
+    numeros_na = [0]*len(dataset[0])
+    for d in dataset:
+        for i in range(len(d)):
+            if "na" in d[i]:
+                numeros_na[i]+=1
 
-for i in range(len(numeros_na)):
-    if (numeros_na[i]/len(dataset))>0.7:
-        deberiamos_quitarla[i]=True
+    for i in range(len(numeros_na)):
+        if (numeros_na[i]/len(dataset))>0.7:
+            deberiamos_quitarla[i]=True
 
 
-indices =[]
-for deberiamos,index in zip(deberiamos_quitarla,range(len(deberiamos_quitarla))):
-    if deberiamos:
-        indices.append(index)
-        print("Debemos quitar la característica " + str(index))
-print("")
+    indices =[]
+    for deberiamos,index in zip(deberiamos_quitarla,range(len(deberiamos_quitarla))):
+        if deberiamos:
+            indices.append(index)
+            print("Debemos quitar la característica " + str(index))
+    print("")
 
-dataset1 = np.delete(dataset,indices,1)
-print("Número de características original: " + str(len(dataset[0])))
-print("Número de características nuevo: " + str(len(dataset1[0])))
+    dataset1 = np.delete(dataset,indices,1)
+    print("Número de características original: " + str(len(dataset[0])))
+    print("Número de características nuevo: " + str(len(dataset1[0])))
+    return dataset1
 
-instancias_malas = []
-for i in range(len(dataset1)):
-    num_nas = 0
-    for v in dataset1[i]:
-        if "na" in v:
-            num_nas+=1
-    if num_nas/len(dataset1[i])>0.15 and dataset[i][0]=="neg":
-        instancias_malas.append(i)
+def eliminaInstanciasMalas(dataset1):
+    instancias_malas = []
+    for i in range(len(dataset1)):
+        num_nas = 0
+        for v in dataset1[i]:
+            if "na" in v:
+                num_nas+=1
+        if num_nas/len(dataset1[i])>0.15 and dataset1[i][0]=="neg":
+            instancias_malas.append(i)
 
-print("\n\n\nTenemos " + str(len(instancias_malas)) + " instancias negativas con más de un 15% de NAs")
+    print("\n\n\nTenemos " + str(len(instancias_malas)) + " instancias negativas con más de un 15% de NAs")
 
-dataset2 = np.delete(dataset1,instancias_malas,0)
-del dataset
-print("Número de instancias del dataset1: " + str(len(dataset1)))
-print("Número de instancias del dataset nuevo: " + str(len(dataset2)))
-print("Número de instancias negativas: " + str(len([d for d in dataset2 if d[0]=="neg"])))
-print("Número de instancias positivas: " + str(len([d for d in dataset2 if d[0]=="pos"])))
-del dataset1
+    dataset2 = np.delete(dataset1,instancias_malas,0)
+    print("Número de instancias del dataset1: " + str(len(dataset1)))
+    print("Número de instancias del dataset nuevo: " + str(len(dataset2)))
+    print("Número de instancias negativas: " + str(len([d for d in dataset2 if d[0]=="neg"])))
+    print("Número de instancias positivas: " + str(len([d for d in dataset2 if d[0]=="pos"])))
+    return dataset2
 
-print("Convertimos el dataset a numérico con formato numpy")
-# Convertimos los datos a formato numpy
-for i in range(len(dataset2)):
-    nueva_fila = []
-    for v in dataset2[i]:
-        if "na" in v:
-            nueva_fila.append(np.nan)
-        #CONVIERTO LAS ETIQUETAS A NÚMEROS
-        elif 'neg' in v:
-            nueva_fila.append(0)
-        elif 'pos' in v:
-            nueva_fila.append(1)
-        else:
-            nueva_fila.append(float(v))
-    dataset2[i]=nueva_fila
+def convierteNumpy(dataset2):
+    print("Convertimos el dataset a numérico con formato numpy")
+    # Convertimos los datos a formato numpy
+    for i in range(len(dataset2)):
+        nueva_fila = []
+        for v in dataset2[i]:
+            if "na" in v:
+                nueva_fila.append(np.nan)
+            #CONVIERTO LAS ETIQUETAS A NÚMEROS
+            elif 'neg' in v:
+                nueva_fila.append(0)
+            elif 'pos' in v:
+                nueva_fila.append(1)
+            else:
+                nueva_fila.append(float(v))
+        dataset2[i]=nueva_fila
+    return dataset2
 
-# Vamos a hacer dos tipos de imputación de valores
-print("Hacemos imputación de valores con la media")
-dataset_media = []
-imp = SimpleImputer(missing_values=np.nan, strategy='mean')
-dataset_media = imp.fit_transform(dataset2)
+def imputaDatos(dataset2, imputacion="mediana"):
+    # Vamos a hacer dos tipos de imputación de valores
+    dataset_imputado=[]
+    if imputacion=="media":
+        print("Hacemos imputación de valores con la media")
+        imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+        dataset_imputado = imp.fit_transform(dataset2)
+    elif imputacion=="mediana":
+        print("Hacemos imputación de valores con la mediana")
+        imp = SimpleImputer(missing_values=np.nan, strategy='median')
+        dataset_imputado = imp.fit_transform(dataset2)
+    else:
+        print("Ese no es un método de imputacion")
+        dataset_imputado=dataset2
+    return dataset_imputado
 
-print("Hacemos imputación de valores con la mediana")
-dataset_mediana = []
-imp = SimpleImputer(missing_values=np.nan, strategy='median')
-dataset_mediana = imp.fit_transform(dataset2)
+def visualizaDatos(dataset,labels):
+    # Pinto el dataset de la mediana en dos dimensiones
+    dataset_reduced = TSNE(n_components=2).fit_transform(dataset)
+    neg = np.array([dataset_reduced[i] for i in range(len(dataset_reduced)) if labels[i]==0])
+    pos = np.array([dataset_reduced[i] for i in range(len(dataset_reduced)) if labels[i]==1])
+    plt.scatter(neg[:,0], neg[:,1], c="green", label="Negativo")
+    plt.scatter(pos[:,0], pos[:,1], c="red", label="Positivo")
+    plt.legend()
+    plt.title("Conjunto de datos")
+    plt.show()
 
-print("Visualizamos en primer lugar el dataset obtenido con la media")
-labels = dataset_media[:,0]
+def obtenerDatosTrain(fichero="../dataset/aps_failure_training_set.csv", imputacion="mediana"):
+    dataset = leeFichero(fichero)
+    dataset1 = eliminaCaracteristicasMalas(dataset)
+    dataset2 = eliminaInstanciasMalas(dataset1)
+    dataset3 = convierteNumpy(dataset2)
+    dataset4 = imputaDatos(dataset3, imputacion=imputacion)
+    labels = dataset4[:,0]
+    dataset = dataset4[:,1:]
+    return dataset,labels
 
-# Pinto el dataset de la media en dos dimensiones
-'''
-dataset_media_reduced = TSNE(n_components=2).fit_transform(dataset_media)
-neg = np.array([dataset_media_reduced[i] for i in range(len(dataset_media_reduced)) if labels[i]==0])
-pos = np.array([dataset_media_reduced[i] for i in range(len(dataset_media_reduced)) if labels[i]==1])
-plt.scatter(neg[:,0], neg[:,1], c="green", label="Negativo")
-plt.scatter(pos[:,0], pos[:,1], c="red", label="Positivo")
-plt.legend()
-plt.title("Conjunto de datos imputados con la media")
-plt.show()
-'''
-
-# Pinto el dataset de la mediana en dos dimensiones
-dataset_mediana_reduced = TSNE(n_components=2).fit_transform(dataset_mediana)
-neg = np.array([dataset_mediana_reduced[i] for i in range(len(dataset_mediana_reduced)) if labels[i]==0])
-pos = np.array([dataset_mediana_reduced[i] for i in range(len(dataset_mediana_reduced)) if labels[i]==1])
-plt.scatter(neg[:,0], neg[:,1], c="green", label="Negativo")
-plt.scatter(pos[:,0], pos[:,1], c="red", label="Positivo")
-plt.legend()
-plt.title("Conjunto de datos imputados con la mediana")
-plt.show()
+def obtenerDatosTest(fichero="../dataset/aps_failure_test_set.csv", imputacion="mediana"):
+    dataset = leeFichero(fichero)
+    dataset1 = eliminaCaracteristicasMalas(dataset)
+    dataset2 = convierteNumpy(dataset1)
+    dataset3 = imputaDatos(dataset2, imputacion=imputacion)
+    labels = dataset3[:,0]
+    dataset = dataset3[:,1:]
+    return dataset,labels
