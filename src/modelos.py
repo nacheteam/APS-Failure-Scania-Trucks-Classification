7ad4d8fb86c50cd4624885e93782e423ad6dd7af
Ignacio Aguilera Martos <nacheteam1996@gmail.com>
2019-06-07 12:42:13 +0200

    Modelos comentados

--- src/modelos.py
+++ src/modelos.py
@@ -17,20 +17,19 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import metrics
 from sklearn import preprocessing
 
+# Fijamos la semilla
 np.random.seed(123456789)
 
-#
-# plot_confusion_matrix
-# @brief: Función encargada de computar y preparar la impresión de la matriz de confusión. Se puede extraer los resultados normalizados o sin normalizar. Basada en un ejemplo de scikit-learn
-# @param: y_true. Etiquetas verdaderas
-# @param: y_pred. Etiquetas predichas
-# @param: classes. Distintas clases del problema (vector)
-# @param: normalize. Booleano que indica si se normalizan los resultados o no
-# @param: title. Título del gráfico
-# @param: cmap. Paleta de colores para el gráfico
-#
-
 def plot_confusion_matrix(y_true, y_pred, classes,normalize=False,title=None,cmap=plt.cm.Blues):
+    '''
+    @brief Función encargada de computar y preparar la impresión de la matriz de confusión. Se puede extraer los resultados normalizados o sin normalizar. Basada en un ejemplo de scikit-learn
+    @param y_true Etiquetas verdaderas
+    @param y_pred Etiquetas predichas
+    @param classes Distintas clases del problema (vector)
+    @param normalize Booleano que indica si se normalizan los resultados o no
+    @param title Título del gráfico
+    @param cmap Paleta de colores para el gráfico
+    '''
     if not title:
         if normalize:
             title = 'Matriz de confusión normalizada'
@@ -52,7 +51,6 @@ def plot_confusion_matrix(y_true, y_pred, classes,normalize=False,title=None,cma
     fig, ax = plt.subplots()
     im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
     ax.figure.colorbar(im, ax=ax)
-    # We want to show all ticks...
     ax.set(xticks=np.arange(cm.shape[1]),
            yticks=np.arange(cm.shape[0]),
            xticklabels=classes, yticklabels=classes,
@@ -88,55 +86,119 @@ def ajustaSVM(dataset_train,labels_train):
     return clasificador_svm
 
 def ajustaRandomForest(dataset_train, labels_train):
-    # Resultado del GridSearch
+    '''
+    @brief Función que ajusta un modelo Random Forest
+    @param dataset_train Conjunto de datos de entrenamiento
+    @para labels_train Etiquetas del conjunto de entrenamiento
+    @return Devuelve el clasificador Random Forest ajustado
+    '''
+    # Parámetros obtenidos por un GridSearch
     clf = RandomForestClassifier(n_estimators=189, random_state=123456789, verbose=True)
     clf.fit(dataset_train, labels_train)
     return clf
 
 def ajustaSGD(dataset_train, labels_train):
+    '''
+    @brief Función que ajusta un modelo Gradiente Descendente Estocástico
+    @param dataset_train Conjunto de datos de entrenamiento
+    @para labels_train Etiquetas del conjunto de entrenamiento
+    @return Devuelve el clasificador Gradiente Descendente Estocástico ajustado
+    '''
+    # Ajustamos el SGD con 10.000 iteraciones como máximo y una tolerancia de 10^-6
     clf = linear_model.SGDClassifier(max_iter=10000, tol=1e-6, verbose=True)
     clf.fit(dataset_train, labels_train)
     return clf
 
 def ajustaBoosting(dataset_train, labels_train):
+    '''
+    @brief Función que ajusta un modelo AdaBoost
+    @param dataset_train Conjunto de datos de entrenamiento
+    @para labels_train Etiquetas del conjunto de entrenamiento
+    @return Devuelve el clasificador AdaBoost ajustado
+    '''
+    # Ajustamos AdaBoost con 100 estimadores
     clf = AdaBoostClassifier(n_estimators=100, random_state=123456789)
     clf.fit(dataset_train,labels_train)
     return clf
 
-def  ajustaRedNeuronal(dataset_train, labels_train):
+def ajustaRedNeuronal(dataset_train, labels_train):
+    '''
+    @brief Función que ajusta un modelo Perceptrón multicapa
+    @param dataset_train Conjunto de datos de entrenamiento
+    @para labels_train Etiquetas del conjunto de entrenamiento
+    @return Devuelve el clasificador Perceptrón multicapa ajustado
+    '''
+    # Perceptrón multicapa con 3 capas ocultas y 100 unidades por capa. Máximo de iteraciones 10.000
     clf = MLPClassifier(hidden_layer_sizes=(100,100,100), max_iter=10000, random_state=123456789, verbose=True)
     clf.fit(dataset_train, labels_train)
     return clf
 
 def cota_eout_test(etest,N,tol):
+    '''
+    @brief Función que calcula la cota de Eout en función de etest
+    @param etest Error obtenido en el conjunto de test
+    @param N número de datos
+    @param tol tolerancia o delta
+    @return Devuelve el valor de la cota
+    '''
     return etest + np.sqrt((1/(2*N))*np.log(2/tol))
 
 def cota_eout_ein(ein,N,tol,vc):
-    return ein + np.sqrt((8/N)*np.log((4*((2*N)**(vc)+1))/tol)
+    '''
+    @brief Función que calcula la cota de Eout en función de ein
+    @param ein Error cometido en el conjunto de train
+    @param N número de datos
+    @param tol tolerancia o delta
+    @param vc Dimensión de vapnik chervonenkis
+    @return Devuelve el valor de la cota
+    '''
+    return ein + np.sqrt((8/N)*np.log((4*((2*N)**(vc)+1))/tol))
 
 def obtenScores(clasificador, dataset_test, labels_test, dataset_train, labels_train, nombre="SGD"):
+    '''
+    @brief Función que toma un clasificador y obtiene las distintas métricas y gráficas que
+    evalúan su resultado en la predicción sobre el conjunto de test
+    @param clasificador Clasificador que queremos evaluar
+    @param dataset_test Conjunto de test
+    @param labels_test Etiquetas del conjunto de test
+    @param dataset_train Conjunto de entrenamiento
+    @param labels_train Etiquetas del conjunto de entrenamiento
+    @param nombre Nombre que saldrá en las gráficas
+    '''
+    # Obtenemos la predicción sobre el conjunto de test con el clasificador
     pred = clasificador.predict(dataset_test)
+    # Obtenemos el score de la predicción anterior
     score = clasificador.score(dataset_test, labels_test)
+    # Obtenemos la predicción sobre el conjunto de train
     pred_in = clasificador.predict(dataset_train)
+
+    # Prints de resultados
     print("Con el fichero de TEST")
     print("El score es: " + str(score))
     print("Precision: " + str(metrics.precision_score(labels_test,pred,average='weighted')))
     print("Recall: " + str(metrics.recall_score(labels_test,pred,average='weighted')))
     print("F1 Score: " + str(metrics.f1_score(labels_test,pred,average="weighted")))
     print("#####################################################################")
+    # Calculamos la estimación de eout y ein
     ein = 1-metrics.accuracy_score(labels_train, pred_in)
     etest = 1-metrics.accuracy_score(labels_test, pred)
     print("Ein: " + str(ein))
     print("Etest: " + str(etest) + "\n\n")
+
+    # Si el modelo es SGD o SVM entonces calculamos las cotas
     if nombre == "SGD":
         print("Cota Eout (con Ein): " + str(cota_eout_ein(ein,len(dataset_train),0.05,3)))
     elif nombre == "SVM":
         print("Cota Eout (con Ein): " + str(cota_eout_ein(ein,len(dataset_train),0.05,len(dataset_train[0]))))
     print("Cota Eout (con Etest): " + str(cota_eout_test(etest,len(dataset_train),0.05)))
     print("Matriz de confusión")
+
+    # Pintamos la matriz de confusión
     nombres = ["neg","pos"]
     plot_confusion_matrix(labels_test, pred, classes=nombres,normalize = False,title='Matriz de confusión para ' + nombre)
     plt.show()
+
+    # Pintamos la curva ROC
     print("Área bajo la curva ROC")
     fpr, tpr, threshold = metrics.roc_curve(labels_test,pred)
     roc_auc = metrics.auc(fpr,tpr)
@@ -151,13 +213,13 @@ def obtenScores(clasificador, dataset_test, labels_test, dataset_train, labels_t
     plt.show()
 
 
+# Leemos los conjuntos de datos
 dataset_train, labels_train, dataset_test, labels_test = preprocesamiento.obtenerDatos(imputacion="mediana")
-
 
 # Random forest
 clf = ajustaRandomForest(dataset_train, labels_train)
 obtenScores(clf,dataset_test, labels_test, dataset_train, labels_train,nombre="Random Forest")
-'''
+
 # Gradiente descendente estocástico
 clf = ajustaSGD(dataset_train, labels_train)
 obtenScores(clf,dataset_test, labels_test, dataset_train, labels_train, nombre = "SGD")
@@ -175,6 +237,7 @@ clf = ajustaRedNeuronal(dataset_train, labels_train)
 obtenScores(clf, dataset_test, labels_test, dataset_train, labels_train nombre="Red Neuronal")
 
 
+'''
 # Grid search para Random Forest
 random_forest = RandomForestClassifier(random_state=123456789)
 parametros = {'n_estimators': np.round(np.linspace(100,200,10)).astype(int)}
